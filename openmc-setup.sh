c9bdb872b7bdcbf83da3d315eb4f7371a888cc13
saad589 <39567015+saad589@users.noreply.github.com>
2023-02-12 20:34:20 +0600

    add exception handling

--- openmc-setup.sh
+++ openmc-setup.sh
@@ -30,17 +30,25 @@
 # Note that the cross-section tables are limited to the following six material temperatures:
 # 250 K, 293.6 K, 600 K, 900 K, 1200 K, and 2500 K. For additional cross-section table at different
 # temperature points (for example, neutronics-TH coupled analysis or neuclear thermal rocket
-# design), please contact DU-ARDS.
+# design), please use contact DU-ARDS.
 #
 # For instruction visit the release,
 # https://github.com/du-ards/openmc-setup
 #
 #
-# Todo: exception handling (symlink already exists; cmake server down; xs repo suffix prob)
-#
-echo
+# Todo: exception handling 
+#    symlink already exists; done 
+#    cmake server down; done 
+#    xs repo suffix; done 
+#    git privilege issue on win 22h2 wsl2;  
+#    set -e global exit on any error disabled;
+# 
+echo 
 echo "*** DU-ARDS OpenMC setup script ***"
-echo
+echo "Script version 0.0.2"
+echo "Git SHA1: d7fc8e58a27fd2569475b7ea25d89a2345c6cb03"
+echo "Copyright (c) 2023; DU-ARDS All rights reserved."
+echo "GNU/GPL license at <https://github.com/du-ards/openmc-setup/blob/main/LICENSE>"
 dir=$PWD
 dirPath="openmc"
 # Make the path to library directory absolute
@@ -60,26 +68,88 @@ then
 else
     echo
     echo "*** Dir check complete ***"
+    echo
 fi
+echo
 echo "*** Distro info ***"
+echo
 cat /etc/*-release
 echo
 echo "*** Intalling dependencies ***"
 echo
 sudo apt-get update && sudo apt-get upgrade -y 
 sudo apt-get install g++ cmake libhdf5-dev libpng-dev libomp-dev libomp5 mpich libmpich-dev git -y
+echo
 echo "*** Cloning into openmc-dev ***"
-cd $dir
+echo
+cd $dir # just pwd 
+mkdir openmc
+# sudo chmod -R 1777 openmc
+chown saad589 openmc
 git clone https://github.com/openmc-dev/openmc.git
+if [ $? -eq 0 ]
+then
+echo 
+echo "*** Cloning into openmc-dev successful ***"
+echo 
+else
+echo "OpenMC cloning failed! This is probably due to one of the following reasons: "
+echo "1. Internet connection issue"
+echo "2. Git couldn't resolve file ownership privileges on the local machine; this is a recurring issue on a Win11 WSL machine when attempting cloning on an /mnt drive. Either fix git ownership privileges or try installing on your ~ directory."
+echo 
+echo "*** Setup failed! ***"
+echo 
+exit 1
+fi
 cd openmc
 git checkout develop
 mkdir build && cd build
 cmake -DOPENMC_USE_OPENMP=on ..
+if [ $? -eq 0 ]
+then
+echo 
+echo "*** Cmake configuration successful ***"
+echo 
+else
+echo "Cmake configuration failed! This is probably due to one of the following reasons: "
+echo "1. Unresolved dependencies"
+echo 
+echo "*** Setup failed! ***"
+echo 
+exit 1
+fi
 make
-echo "*** Compilation done! ****"
-echo
+if [ $? -eq 0 ]
+then
+echo 
+echo "*** Compilation successful  ***"
+echo 
+else
+echo "Compilation failed! This is probably due to one of the following reasons: "
+echo "1. Cmake profiling error"
+echo "2. Unresolved dependencies"
+echo "3. Incompatible compiler"
+echo 
+echo "*** Setup failed! ***"
+echo 
+exit 1
+fi
 echo "*** Creating symbolic link ***"
 sudo ln -s $dir/openmc/build/bin/openmc /usr/bin/openmc
+if [ $? -eq 0 ]
+then
+echo 
+echo "*** Symbolic link creation successful ***"
+echo 
+else
+echo 
+echo "Symbolic link creation failed! This is probably due to one of the following reasons: "
+echo "1. Symbolic link already exists with the same name in /usr/bin"
+echo
+echo "*** Setup failed! ***"
+echo 
+exit 1
+fi
 cd ..
 echo
 echo "*** Installing the Python API and dependencies ***"
@@ -88,18 +158,44 @@ sudo -H pip3 install --upgrade pip
 sudo pip install numpy vtk jupyter
 sudo pip install .
 cd $dir
+echo
 echo "*** API installation complete ***"
 echo
 echo "*** Downloading cross-section libraries ***"
 wget https://anl.box.com/shared/static/uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz
+if [ $? -eq 0 ]
+then
+echo 
+echo "*** Library download successful ***"
+echo 
+else
+echo 
+echo "Dowload failed! This is probably due to one of the following reasons: "
+echo "1. Link is not valid"
+echo "2. Internet issue"
+echo
+echo "*** Setup failed! ***"
+echo 
+exit 1
+fi
 echo
 echo "*** Extracting ***"
+echo
 tar xf uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz
+rm uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz
 echo 
 echo "*** Setting up environment variables ***"
+echo
 echo "export OPENMC_CROSS_SECTIONS='$dir/endfb-viii.0-hdf5/cross_sections.xml'" >> $HOME/.bashrc
 source $HOME/.bashrc
 cd $dir
-echo "*** OpenMC setup done!****"
-echo "Please try the following: "
+echo
+echo "*** OpenMC setup done! ***"
+echo 
+echo "*** Exiting DU-ARDS OpenMC setup script ***"
+echo
+echo "Please try the following commands: "
+echo
+echo "echo $OPENMC_CROSS_SECTIONS"
 echo "openmc --version"
+
